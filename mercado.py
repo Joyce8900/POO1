6e036cb4cfe59f3ae0caf41fa307d41ac23aef04
Joyce <joycesantos8900@gmail.com>
2023-03-13 16:22:47 -0300

    Implementando função de formatação de horas.

--- mercado.py
+++ mercado.py
@@ -1,18 +1,18 @@
 from datetime import datetime
-import random
+from random import randint
 
 def gerar_data():
-    ano = random.randint(2023, 2024)
-    mes = random.randint(1, 12)
+    ano = randint(2023, 2024)
+    mes = randint(1, 12)
     max_dias = 31 if mes in [1, 3, 5, 7, 8, 10, 12] else 30 if mes in [4, 6, 9, 11] else 29 if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0) else 28
-    dia = random.randint(1, max_dias)
-    return f"{dia:02d}/{mes:02d}/{ano}"
+    dia = randint(1, max_dias)
+    return (f"{dia:02d}/{mes:02d}/{ano}")
 
 
-class DataTime:
-    @staticmethod
-    def now():
-        return datetime.now()
+def DataTime():
+    data_e_hora_atuais = datetime.now()
+    data_formatada = data_e_hora_atuais.strftime("%d/%m/%Y %H:%M:%S")
+    return (data_formatada)
 
 
 class Produto:
@@ -35,7 +35,7 @@ class Item:
 
     def __str__(self):
         return f"Quantidade: {self.quantidade} -- Produto: {self.produto}" \
-               f" -- Total: {self.total()}"
+               f"Total: {self.total()}"
 
 
 class Venda:
@@ -56,12 +56,27 @@ class Venda:
 
 
 if __name__ == "__main__":
-    chocolate = Produto(10.50, "Chocolate", gerar_data())
+    
+    chocolate = Produto(preco = 10.50, descricao=   "Chocolate", validade= gerar_data())
     pacote = Item(2, chocolate)
-    venda = Venda(DataTime.now(), [pacote])
-    macarrao = Produto(2.50, "Macarrão", gerar_data())
-    pacote1 = Item(5, macarrao)
-    venda= Venda(DataTime.now(),[pacote1])
+    venda = Venda(DataTime(), [pacote])
 
+    carne = Produto(preco = 34.99, descricao= "Carne", validade = gerar_data())
+    pacote = Item(2, carne)
     venda.set_item(pacote.quantidade, pacote.produto)
+
+    macarrao = Produto(preco = 3.75, descricao="Macarrão",validade= gerar_data())
+    pacote = Item(5, macarrao)
+    venda.set_item(pacote.quantidade, pacote.produto)
+
+    suco_laranja = Produto(preco=5.00, descricao="Suco de Laranja", validade = gerar_data())
+    pacote = Item(3, suco_laranja)
+    venda.set_item(pacote.quantidade, pacote.produto)
+
+    biscoito = Produto(preco=3.29, descricao = "Biscoito", validade=gerar_data())
+    pacote = Item(1, biscoito)
+    venda.set_item(pacote.quantidade, pacote.produto)
+
+
+
     print(venda)
