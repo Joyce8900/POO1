68f6d459e1a9de12f979a573d81593b4899bf0a8
Joyce <joycesantos8900@gmail.com>
2023-03-30 15:49:47 -0300

    Receita1 finalizada

--- receita1/lib/main.dart
+++ receita1/lib/main.dart
@@ -12,21 +12,41 @@ void main() {
           title: Text("Receita1"),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: (){},
-            child:
-              Text(
-                "Viva Santana",
-                style: TextStyle(
-                fontSize: 20, 
-                fontFamily: 'Roboto', 
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic, 
-                color: Colors.white, 
-              ),)
-             
-          )
-        )
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 50), 
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  child: Text(
+                    "Botão 01",
+                    style: TextStyle(
+                      fontSize: 20, 
+                      fontFamily: 'Roboto', 
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic, 
+                      color: Colors.white, 
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(Size.fromHeight(50)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.zero))
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: IconButton(
+                  icon: Icon(Icons.favorite),
+                  onPressed: () {},
+                  color: Colors.black,
+                  iconSize: 40,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
