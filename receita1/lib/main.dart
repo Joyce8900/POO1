1f0b2543d14180db0d7b9750b6a6f663f5b6526d
Joyce <joycesantos8900@gmail.com>
2023-04-12 13:35:29 -0300

    correção de bug

--- receita1/lib/main.dart
+++ receita1/lib/main.dart
@@ -1,61 +1,73 @@
-import 'package:flutter/material.dart';
+mport 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minha Aplicação',
+void main() {
+  runApp(
+    MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Minha Aplicação'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Você pressionou o botão este número de vezes:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Receita1"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              SizedBox(height: 50),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  child: Text(
+                    "Botão 01",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(Size.fromHeight(50)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.zero))
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: IconButton(
+                  icon: Icon(Icons.favorite),
+                  onPressed: () {},
+                  iconSize: 40,
+                ),
+              ),
+              SizedBox(height: 50),
+              Image.network(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-GIkHyzeKbrGUEt_f4DXVu4MpPz0X4AGHqUsC0Puj&s',
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                          : null,
+                    ),
+                  );
+                },
+                errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                  return Text('Imagem indisponível');
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Incrementar',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+    ),
+  );
