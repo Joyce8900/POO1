3928fc7834dded221318482dd152b1181795831b
Joyce Santos <92751739+Joyce8900@users.noreply.github.com>
2023-05-03 15:10:24 -0300

    Update main.dart

--- tarefa6/lib/main.dart
+++ tarefa6/lib/main.dart
@@ -1,157 +1,223 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo de Formulário em Flutter',
-      home: FormScreen(),
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Inputs"),
+          ),
+          body: const MyCustomForm(),
+          bottomNavigationBar: NewNavBar(icons: const [
+            Icons.ac_unit,
+            Icons.access_alarm,
+            Icons.accessibility
+          ]),
+        ));
+  }
+}
+
+class NewNavBar extends StatelessWidget {
+  final List icons;
+  NewNavBar({required this.icons});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        items: icons
+            .map((data) =>
+                BottomNavigationBarItem(icon: Icon(data), label: "Teste"))
+            .toList());
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Creating the input field for the name
+          Padding(
+            padding: const EdgeInsets.all(16.0), // Add padding here
+            child: MyNameInput(),
+          ),
+          // Creating the input field for the password
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: MyPasswordInput(),
+          ),
+          // Creating the DropdownButton field
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: MyDropdownInput(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: MySlideInput(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('It all works"')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class FormScreen extends StatefulWidget {
-  @override
-  _FormScreenState createState() => _FormScreenState();
-}
-
-class _FormScreenState extends State<FormScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String _name = '';
-  String _email = '';
-  String _favoriteColor = 'Vermelho';
-  bool _receiveNotifications = false;
-  double _priceRange = 50.0;
+class MyNameInput extends StatelessWidget {
+  MyNameInput();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Demo de Formulário em Flutter'),
+    return TextFormField(
+      validator: (value) {
+        String patttern = r'(^[a-zA-Z ]*$)';
+        RegExp regExp = RegExp(patttern);
+        if (value == null || value.isEmpty) {
+          return "Please input your name";
+        } else if (!regExp.hasMatch(value)) {
+          return "O nome deve conter caracteres de a-z ou A-Z";
+        }
+      },
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Enter text here',
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('Nome'),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Por favor, insira seu nome';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() {
-                    _name = value;
-                  });
-                },
-              ),
-              SizedBox(height: 16.0),
-              Text('Email'),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Por favor, insira seu email';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() {
-                    _email = value;
-                  });
-                },
-              ),
-              SizedBox(height: 16.0),
-              Text('Cor Favorita'),
-              Row(
-                children: <Widget>[
-                  Radio(
-                    value: 'Vermelho',
-                    groupValue: _favoriteColor,
-                    onChanged: (value) {
-                      setState(() {
-                        _favoriteColor = value;
-                      });
-                    },
-                  ),
-                  Text('Vermelho'),
-                  Radio(
-                    value: 'Verde',
-                    groupValue: _favoriteColor,
-                    onChanged: (value) {
-                      setState(() {
-                        _favoriteColor = value;
-                      });
-                    },
-                  ),
-                  Text('Verde'),
-                  Radio(
-                    value: 'Azul',
-                    groupValue: _favoriteColor,
-                    onChanged: (value) {
-                      setState(() {
-                        _favoriteColor = value;
-                      });
-                    },
-                  ),
-                  Text('Azul'),
-                ],
-              ),
-              SizedBox(height: 16.0),
-              Text('Receber Notificações'),
-              Switch(
-                value: _receiveNotifications,
-                onChanged: (value) {
-                  setState(() {
-                    _receiveNotifications = value;
-                  });
-                },
-              ),
-              SizedBox(height: 16.0),
-              Text('Faixa de Preço'),
-              Slider(
-                value: _priceRange,
-                min: 0,
-                max: 100,
-                divisions: 10,
-                onChanged: (value) {
-                  setState(() {
-                    _priceRange = value;
-                  });
-                },
-              ),
-              SizedBox(height: 32.0),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Enviando Formulário...'),
-                        ),
-                      );
-                    }
-                  },
-                  child: Text('Enviar'),
-                ),
-              ),
-            ],
+    );
+  }
+}
+
+class MyPasswordInput extends StatefulWidget {
+  @override
+  _MyPasswordInputState createState() => _MyPasswordInputState();
+}
+
+class _MyPasswordInputState extends State<MyPasswordInput> {
+  bool showPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a passport';
+        }
+        return null;
+      },
+      obscureText: !showPassword,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: 'Enter passport here',
+        hintText: 'Enter your password',
+        suffixIcon: IconButton(
+          icon: Icon(
+            showPassword ? Icons.visibility : Icons.visibility_off,
           ),
-  ),
-                ),
-              ),
-            ],
-          ),
+          onPressed: () {
+            setState(() {
+              showPassword = !showPassword;
+            });
+          },
         ),
       ),
     );
   }
 }
 
-       
+class MyDropdownInput extends StatefulWidget {
+  @override
+  _MyDropdownInputState createState() => _MyDropdownInputState();
+}
+
+class _MyDropdownInputState extends State<MyDropdownInput> {
+  String? _opcao;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<String>(
+      decoration: const InputDecoration(labelText: 'Role'),
+      value: _opcao,
+      onChanged: (newValue) {
+        setState(() {
+          _opcao = newValue;
+        });
+      },
+      items: const [
+        DropdownMenuItem(
+          value: 'Back-End',
+          child: Text('Back-End'),
+        ),
+        DropdownMenuItem(
+          value: 'Front-End',
+          child: Text('Front-End'),
+        ),
+      ],
+      validator: (value) {
+        if (value == null) {
+          return 'Please select a option';
+        }
+        return null;
+      },
+    );
+  }
+}
+
+class MySlideInput extends StatefulWidget {
+  @override
+  _MySlideInputState createState() => _MySlideInputState();
+}
+
+class _MySlideInputState extends State<MySlideInput> {
+  double _curentSliderValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: _curentSliderValue,
+      max: 100,
+      divisions: 5,
+      label: _curentSliderValue.round().toString(),
+      onChanged: (double newValue) {
+        setState(() {
+          _curentSliderValue = newValue;
+        });
+      },
+    );
+  }
+}
